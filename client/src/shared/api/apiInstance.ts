bd95d07fab14502a02601acc8dbb05d33dceaefd
paul <p@x128.ru>
2024-01-26 22:09:25 +0300

    auth fix

--- client/src/shared/api/apiInstance.ts
+++ client/src/shared/api/apiInstance.ts
@@ -4,6 +4,10 @@ import { authLocalStore } from '../../auth/local-store/authLocalStore';
 export const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_SERVER_URL,
     headers: {
-        Authorization: `Bearer ${authLocalStore.getAuthToken()}`,
+        common: {
+            Authorization: `Bearer ${authLocalStore.getAuthToken()}`,
+        },
     },
 });
+
+// axiosInstance.defaults.headers['Authorization'] = `Bearer ${authLocalStore.getAuthToken()}`;
